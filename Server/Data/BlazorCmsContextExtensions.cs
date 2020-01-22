283a3888434a7df7ee7d7421a324132122b898a8
Mat Jones <mjones@andculture.com>
2020-01-22 19:12:22 +0000

    update seeded article

--- Server/Data/BlazorCmsContextExtensions.cs
+++ Server/Data/BlazorCmsContextExtensions.cs
@@ -20,7 +20,7 @@ namespace BlazorCMS.Server.Data
                     new Article
                     {
                         Title = "Hello World!",
-                        Body  = "### Yo\n\n[This](www.google.com) is a link to Google.\n\n- This\n- Is\n- A\n- Bulleted\n- List\n\n```c#\nnamespace BlazorCMS {\npublic class TestClass {\npublic void PrintMessage() => Console.WriteLine(\"This is C# code!\");\n}\n}\n```\n\n`Console.WriteLine(\"This is inline code!\");`\n\n<div style=\"background-color: black; color: white;\">\nThis is an HTML div with custom inline styles!\n</div>"
+                        Body  = "### Yo\n\n[This](www.google.com) is a link to Google.\n\n- This\n- Is\n- A\n- Bulleted\n- List\n\n```c#\nnamespace BlazorCMS {\n    public class TestClass {\n        public void PrintMessage() => Console.WriteLine(\"This is C# code!\");\n    }\n}\n```\n\n`Console.WriteLine(\"This is inline code!\");`\n\n<div style=\"background-color: black; color: white;\">\nThis is an HTML div with custom inline styles!\n</div>"
                     }
                 }
             };
