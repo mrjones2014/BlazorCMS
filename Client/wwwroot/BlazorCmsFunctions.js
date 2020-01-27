0bf94b1eba36f940331427003ff66174e557ef4a
Mat Jones <mjones@andculture.com>
2020-01-27 15:33:20 +0000

    change to Prism.js for syntax highlighting

--- Client/wwwroot/BlazorCmsFunctions.js
+++ Client/wwwroot/BlazorCmsFunctions.js
@@ -12,5 +12,21 @@ window.BlazorCmsJsFunctions = {
         }
 
         return el.classList.contains(className);
+    },
+    applySyntaxHighlighting: function (selector) {
+        if (selector == null) {
+            Prism.highlightAll();
+            console.log("HIGHLIGHT ALL!!!");
+            return;
+        }
+
+        var el = document.querySelector(selector);
+        if (el == null) {
+            console.log("EL IS NULL!");
+            return;
+        }
+        Prism.highlightAllUnder(el);
+
+        console.log("done!!!!");
     }
 };
